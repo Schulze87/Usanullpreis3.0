2108118bccfc3ed0ed4e2f46b008da1b976ad9b3
Schulze87 <l.schulze.koenig@tgu-greven.com>
2026-03-05 13:28:10 +0100

    Delete UsaNullpreis/src/Resources/app directory

--- UsaNullpreis/src/Resources/app/administration/src/main.js
+++
@@ -1,7 +0,0 @@
-import './component/usa-nullpreis-customer-group-select';
-import './component/usa-nullpreis-rule-select';
-import deDE from './snippet/de-DE.json';
-import enGB from './snippet/en-GB.json';
-
-Shopware.Locale.extend('de-DE', deDE);
-Shopware.Locale.extend('en-GB', enGB);
