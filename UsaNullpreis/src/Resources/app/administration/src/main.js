184466ac310aa18828c97bdfbd5f2a7953784256
Schulze87 <l.schulze.koenig@tgu-greven.com>
2026-03-05 11:36:33 +0100

    Update main.js

--- UsaNullpreis/src/Resources/app/administration/src/main.js
+++ UsaNullpreis/src/Resources/app/administration/src/main.js
@@ -1,1 +1,6 @@
 import './component/usa-nullpreis-config';
+import deDE from './snippet/de-DE.json';
+import enGB from './snippet/en-GB.json';
+
+Shopware.Locale.extend('de-DE', deDE);
+Shopware.Locale.extend('en-GB', enGB);
