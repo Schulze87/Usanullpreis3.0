3b6fbe1388f3fa877703c2c747e4ebc687e931bb
Schulze87 <l.schulze.koenig@tgu-greven.com>
2026-04-07 14:27:59 +0200

    Delete UsaNullpreis/src/Resources/app/storefront/src directory

--- UsaNullpreis/src/Resources/app/storefront/src/main.js
+++
@@ -1,5 +0,0 @@
-import UsaNullpreisObserverPlugin from './plugin/usa-nullpreis-observer.plugin';
-
-const PluginManager = window.PluginManager;
-
-PluginManager.register('UsaNullpreisObserverPlugin', UsaNullpreisObserverPlugin, 'body');
